d64974d2f7f341c2255fb68b217ff881d5867be2
Your Name <you@example.com>
2019-05-26 04:32:04 +0900

    practice

---
+++ work space/personal/src/personal/Personal_dowhile.java
@@ -0,0 +1,20 @@
+package personal;
+
+import java.util.Scanner;
+
+public class Personal_dowhile {
+	 public static void main(String[] args)
+	 {
+		 Scanner sc=new Scanner(System.in);
+		 int Seven;
+		 
+		 
+		 do {			 
+		     System.out.println("7을 입력해주세요");
+		     int a=sc.nextInt();
+		     Seven=a;
+}
+		 while(Seven!=7);
+
+}
+}

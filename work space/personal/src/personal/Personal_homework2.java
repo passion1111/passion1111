2ee3e68e7a91aca3f5f4b06d412e3c78f2ed3ed9
passion1111 <se0572@naver.com>
2019-05-28 19:28:03 +0900

    practice

--- work space/personal/src/personal/Personal_homework2.java
+++ work space/personal/src/personal/Personal_homework2.java
@@ -6,7 +6,7 @@ public class Personal_homework2 {
 		j=0;
 		for(i=1;i<=10;i++) {
 			d=-i;
-			if(i%2==0)d=i;
+			if(i%2==0) d=i;
 			j=d+j;
 			System.out.println(j);
 		}
--- work space/personal/src/personal/Personal_homework3.java
+++ work space/personal/src/personal/Personal_homework3.java
@@ -2,7 +2,8 @@ package personal;
 
 public class Personal_homework3 {
 	public static void main(String[] args) {
-		double  i,j,sum;
+		
+		double i, j,sum;
 		sum=0;
 		
 			for(i=1;i<=9;i++) {

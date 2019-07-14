3b34f48ac2bd10bbddf5037e7f23f9ead5363c23
passion1111 <se0572@naver.com>
2019-07-14 21:59:12 +0900

    github nodemodule 추가안되게 ignore추가 eslint와 npm사용법을 익히는 중입니다.

--- work space/day06/src/array2/Mainentry_quiz4.java
+++ work space/day06/src/array2/Mainentry_quiz4.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class Mainentry_quiz4 {
 
 	public static void main(String[] args) {
-
+		
 		int[][] arr={{10,20,0},{1,2,0},{5,7,0}};
 		
 		for(int i=0;i<arr.length;i++) {

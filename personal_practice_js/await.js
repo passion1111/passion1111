d6c4d99b77bb46750b6d3b0200e7a3885e29b8cf
passion1111 <se0572@naver.com>
2019-07-16 19:02:09 +0900

    ajax AJAX에대한 예제들 복습이 필요해보인다. 수업시간에 spring boot 설정떄문에 수업에 소홀히 했던것같다.

--- personal_practice_js/await.js
+++ personal_practice_js/await.js
@@ -5,25 +5,66 @@ return new Promise((resolve,reject)=>{
     },sec*1000);
 });
 }
+//--async 예제
+//아래보면 3초뒤에 asyn가 찍힘
+// async function myAsync2() {
+//     await delayP(3);
+//     return 'async';
+// }
+// myAsync2().then((result)=>{
+//     console.log(result);
+// });
+//async2 예제
 
-function myFunc(){
-    return 'func';
-}
+// async function myAsync2() {
+//      const time=  await delayP(3);  //이런식으로  delayP의 결과값을 time으로 집어넣을수 있음.
+//      console.log(time);
+//         return 'async';
+//     }
+//     myAsync2().then((result)=>{
+//         console.log(result);
+//     });
+//-------------------------------
 
-async function myAsync(){
-    delayP(0).then((time)=>{
-        console.log(time+"여긴 그냥임");
+
+// function myFunc(){
+//     return 'func';
+// }
+
+
+//--------------async예제 3 아직 완성되지않음.
+async function myAsync2() {
+     const time=  await delayP(3);  //이런식으로  delayP의 결과값을 time으로 집어넣을수 있음.
+     console.log(time);
+        return 'async';
+    }
+    myAsync2().then((result)=>{
+        console.log(result);
     });
-    // console.log("두번찍히나?");//이해완료 
-    // await...      //await은 promise가 resolve돼서 결과값이 넘어올떄까지 기달린다.
-    return 'async'
-}
-//어싱크에서는 리졸브로 넘기게되는값은 결국 리턴하면 되고
-// 리젝을 해야한다면 에러를 throw해야함. 
 
-myAsync().then((result)=>{//여기서 result는 async 리턴값
- console.log(result);
-});
-console.log(myFunc());
-// console.log(myAsync());
+
+
+
+
+
+
+
+
+
+// async function myAsync(){
+//     delayP(0).then((time)=>{
+//         console.log(time+"여긴 그냥임");
+//     });
+//     // console.log("두번찍히나?");//이해완료 
+//     // await...      //await은 promise가 resolve돼서 결과값이 넘어올떄까지 기달린다.
+//     return 'async'
+// }
+// //어싱크에서는 리졸브로 넘기게되는값은 결국 리턴하면 되고
+// // 리젝을 해야한다면 에러를 throw해야함. 
+
+// myAsync().then((result)=>{//여기서 result는 async 리턴값
+//  console.log(result);
+// });
+// console.log(myFunc());
+// // console.log(myAsync());
 

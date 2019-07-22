d5cacf878560c70db9c2b1655d7ac32ca4023cfb
passion1111 <se0572@naver.com>
2019-07-22 21:01:06 +0900

    방명록 DB 추가하지못했던 sql파일 추가 웹팩에 대한 개념과 태스크러너에대한 개념을 습득했는지 정확히 어떻게 사용해야하는지 찾아봐야겠다.

--- oracle/kim/0709kim.sql
+++ oracle/kim/0709kim.sql
@@ -1,4 +1,35 @@
-select*from board;  --이 테이블은 전에 board2의 테이블 
+drop table guestBook;
+
+create table guestBook (
+	num number,
+	name varchar2(50) not null,
+	email varchar2(50),
+	home varchar2(50),
+	contents varchar2(2000) not null
+) SEGMENT creation IMMEDIATE;  
+
+select * from guestBook;
+
+drop sequence book_num;
+create sequence book_num; 
+
+insert into guestBook(num, name, email, home, contents)
+     values(book_num.nextval, 'dd', 'aa@kosta.or.kr', 'homeaa', 'hello');
+     
+     commit;
+--------------------------------------------------
+ select name, email, home, contents from GUESTBOOK 
+ 				where num > 6 and num <= 23 
+ 				order by num desc;
 
 
-select*from member_board;  -- 이 테이블은 지금 jav_dev 테이블
+ select count(num) from GUESTBOOK;
+ select *from guestbook;
+
+-----------------------------------------------
+
+update guestBook set email='ii@i.kr', home='http://', contents='hi' where num = 1;
+
+update guestBook set email= ? , home= ? , contents= ? where num = ?;
+
+

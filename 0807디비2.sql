fc595a848e63dc2275e58dc364639d809719a460
passion1111 <se0572@naver.com>
2019-08-12 05:52:38 +0900

    아침06시. 분류 선택가능하게 만들고 글자수가 3글자이상이면 ...으로 표시하게 만든것. session처리도 좀 더 신경써야하고 ajax 해싱처리도 해야한다. 다 만들고나서 css적용할것.

--- 0807디비2.sql
+++ 0807디비2.sql
@@ -1,75 +1,16 @@
-select * from tra;
-select * from trarep;
-desc trarep;
-select *  from  (select rownum rnum,tra_num,tra_subject,tra_readcount,tra_date,tra_writer , tra_filename,tra_contents,tra_head,tra_alive from(select * from tra  order by tra_num desc))where 0<=rnum and rnum<=5 and tra_head; 
-commit;
---거래 댓글 테이블
-----------------
-drop table trarep;
-select * from trarep;
-create table trarep(
-    trarep_num number , -- 번호PK
-    trarep_tranum number , -- 질문게시판 번호FK
-    trarep_date date default sysdate, -- 작성날짜
-    trarep_contents varchar2(4000), -- 내용
-    trarep_writer varchar2(120) , -- 아이디FK
-    trarep_writerrep number , -- 부모작성자 번호
-    
-    trarep_alive number default 0, -- 삭제유무
-    trarep_numref number, -- 대댓글 유무
-    trarep_numref_lv number -- 대댓글 순서
+------------
+--회원 테이블
+------------
+select * from mem;
+create table mem(
+    mem_id varchar2(40) constraint mem_id_pk primary key, -- 아이디
+    mem_pwd varchar2(32) not null, -- 비밀번호
+    mem_ph varchar2(32), -- 전화번호
+    mem_name varchar2(120), -- 이름   
+    mem_addr varchar2(200), -- 주소
+    mem_filename varchar2(520), -- 파일이름
+    mem_point number default 100, -- 포인트
+    mem_alive number default 1, -- 탈퇴유무(탈퇴:0)
+    mem_cdate date default sysdate, -- 가입날짜
+    mem_ddate date -- 탈퇴날짜
 );
-select * from trarep where trarep_tranum = 10;
-select * from trarep where trarep_tranum=10 order by trarep_numref,trarep_numref_lv ,trarep_num desc; 
-
-insert into trarep(trarep_num,trarep_tranum,trarep_date,trarep_contents,trarep_writer,trarep_writerrep,trarep_alive,
-trarep_numref,trarep_numref_lv) values(1,1,sysdate,'안녕','qdw',1,0,0,0);  --첫 댓글
-rollback;
-insert into trarep(trarep_num,trarep_tranum,trarep_date,trarep_contents,trarep_writer,trarep_writerrep,trarep_alive,
-trarep_numref,trarep_numref_lv) values(13,10,sysdate,'안녕','첫번쨰',11,0,1,1);
-select*from trarep;
-                                                --글번호                                       --대댓글유무        --레벨
-select *from trarep where trarep_tranum=1 START WITH trarep_writerrep = 0 connect by prior trarep_num=trarep_writerrep order siblings by trarep_num desc;
-
-select *from trarep where  trarep_tranum=10 order by trarep_writerrep , trarep_numref asc ,trarep_numref_lv asc;
-select * from trarep where trarep_tranum=10 start with trarep_numref=0 connect by nocycle prior trarep_numref =trarep_numref_lv order siblings by trarep_num;
-
-select * from trarep  where trarep_tranum=11 order by trarep_writerrep asc,trarep_numref asc,trarep_numref_lv asc,trarep_num asc ;
-select count(*) from trarep where trarep_writerrep=? and trarep_numref=?;
-select *from trarep;
-update trarep set trarep_contents='333' where trarep_num=5;
-
-select *from trarep from(select * from trarep order by trarep_writerrep desc,trarep_numref asc) order by trarep_writerrep desc,trarep_numref asc,trarep_numref_lv asc;
-drop table trarep;
-qarep_num number constraint qarep_num_pk primary key, -- 번호PK
-    qarep_qanum number constraint qarep_qanum_fk references qa(qa_num), -- 질문게시판 번호FK
-    qarep_date date default sysdate, -- 작성날짜
-    qarep_contents varchar2(4000), -- 내용
-    qarep_writer varchar2(120) constraint qarep_writer_fk references mem(mem_id), -- 아이디FK
-    qarep_writerrep number constraint qarep_writerrep_fk references qa(qa_num), -- 부모작성자 번호 
-    qarep_alive number default 0, -- 삭제유무
-    qarep_numref number, -- 대댓글 유무
-    qarep_numref_lv number -- 대댓글 순서
-);
-trarep_num number , -- 번호PK
-    trarep_tranum number , -- 질문게시판 번호FK
-    trarep_date date default sysdate, -- 작성날짜
-    trarep_contents varchar2(4000), -- 내용
-    trarep_writer varchar2(120) , -- 아이디FK
-    trarep_writerrep number , -- 부모작성자 번호
-    
-    trarep_alive number default 0, -- 삭제유무
-    trarep_numref number, -- 대댓글 유무
-    trarep_numref_lv number -- 대댓글 순서
-commit;
-insert into trarep(trarep_num,trarep_tranum,trarep_contents,trarep_writer,trarep_writerrep,trarep_numref,trarep_numref_lv) values(1,1,'1','아이디',1,1,1);
-select* from trarep;
-select count(*) from trarep where trarep_tranum=1 ;
-desc trarep;
-select * from  trarep;
-select count(*) from  trarep where trarep_tranum = 9;
-select *from trarep;
-
-
-select count(*) from tra where tra_head ='전체';
-select *from tra;

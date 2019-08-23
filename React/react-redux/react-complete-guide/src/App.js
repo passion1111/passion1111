e1db4d9969f1315705c5908857c97cc6d8e9d9de
passion1111 <se0572@naver.com>
2019-08-23 20:53:13 +0900

    리액트 state와 props에대한 개념을 배웠는데 리액트 16.8이상버전부터는 컴포넌트를 거의사용하지않아서 리액트훅에대한 사용방법을배웠다. 처음부터 둘다 배울려니 약간 헷갈렸지만 기존 코드를 유지보수할려면 둘다 배워야할것같고. 자바스크립트 최신 코드들 map filter reducer에 대해서 조금 더 배워둬야 할 필요가있다.

--- React/react-redux/react-complete-guide/src/App.js
+++ React/react-redux/react-complete-guide/src/App.js
@@ -10,19 +10,56 @@ import Person from './Person/Person';
 //     </div>
 //   );
 // }
+
+
+//class based
 class App extends Component{
+  state={
+    persons:[
+    {name:"232132",age:28},
+    {name:'manu',age:30},
+    {name:'2322',age:33330}
+    ],
+    otherState:"some other value"
+  }
+  switchNameHandler=(newName)=>{
+    console.log("Was clicked");
+    // 절대 이렇게 쓰지 말것.this.state.persons[0].name="하와아"
+    this.setState({
+      persons:[
+        {name: "첫쨰",age:28},
+        {name:newName,age:30},
+        {name:'세번쨰',age:33330}
+        ]
+
+    })
+  }
+
+  nameChangedHandler=(event)=>{
+    this.setState({
+      persons:[
+        {name: "newName",age:432423},
+        {name: event.target.value,age:30},
+        {name: 'ㅇㅇㅇ',age:33330}
+        ]
+
+    })
+  }
+
   render(){
     return(
       <div className="App">
 <h1>리액트를 확인</h1>
+<Person name={this.state.persons[0].name} age={this.state.persons[0].age}
+ click={this.switchNameHandler.bind(this,"자식한테 보내주자")}
+ changed={this.nameChangedHandler}> 
+ 
+  myhass</Person>
+<Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+<Person name="manu" age="33"/>
 <Person/>
-<Person/>
-<Person/>
-<Person/>
-<Person/>
-<Person/>
-<Person/>
-
+              {/* 함수 ()를 쓰지말것 왜냐하면 함수실행되면 리렌더링되니까 */}
+<button onClick={()=>this.switchNameHandler("요롷게 저렇게")}>눌려라</button>
 
       </div>
     )
@@ -33,4 +70,63 @@ class App extends Component{
 }
 }
 
+
 export default App;
+
+
+
+//리액트 훅 이용
+// import React, { useState } from 'react';
+// import './App.css';
+// import Person from './Person/Person';
+
+// const app = props => {
+//   const [personsState, setPersonsState] = useState({
+//     persons: [
+//       { name: 'Max', age: 28 },
+//       { name: 'Manu', age: 29 },
+//       { name: 'Stephanie', age: 26 }
+//     ]
+//   });
+
+//   const [otherState, setOtherState] = useState('some other value');
+
+//   console.log(personsState, otherState);
+
+//   const switchNameHandler = () => {
+//     // console.log('Was clicked!');
+//     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+//     setPersonsState({
+//       persons: [
+//         { name: 'Maximilian', age: 28 },
+//         { name: 'Manu', age: 29 },
+//         { name: 'Stephanie', age: 27 }
+//       ]
+//     });
+//   };
+
+//   return (
+//     <div className="App">
+//       <h1>Hi, I'm a React App</h1>
+//       <p>This is really working!</p>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Person
+//         name={personsState.persons[0].name}
+//         age={personsState.persons[0].age}
+//       />
+//       <Person
+//         name={personsState.persons[1].name}
+//         age={personsState.persons[1].age}
+//       >
+//         My Hobbies: Racing
+//       </Person>
+//       <Person
+//         name={personsState.persons[2].name}
+//         age={personsState.persons[2].age}
+//       />
+//     </div>
+//   );
+//   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+// };
+
+// export default app;
--- React/react-redux/react-complete-guide/src/Person/Person.js
+++ React/react-redux/react-complete-guide/src/Person/Person.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import './Person.css';
 
-const person= ()=>{
+const person= (props)=>{
     return (
-    <h2>맞아?</h2>
-    )}
-
+    <div className="Person">
+    
+    {/* //   <h2>맞아? 내 이름{props.name}는 랜덤입니다 자바스크립트로~ {Math.floor(Math.random()*30)} 살이에요~</h2> */}
+      <h2 onClick={props.click}>맞아? 내 이름{props.name}는 랜덤입니다 자바스크립트로~ {props.age} 살이에요~</h2>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed}   />
+ {/* //만약 클래스일경우 this.props이렇게 사용하면 됨. */}
+    </div>
+      )     
+    }
+    person.defaultProps={
+        name:"아무개",
+        age:"33"
+    }
 export default person;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // import React from 'react';

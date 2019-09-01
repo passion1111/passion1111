beba235c0449fc90d7ad253bc0f976ac403689af
passion1111 <se0572@naver.com>
2019-09-01 22:30:20 +0900

    npm사용법과 문제들 npm을 시작할려고했는데 react script start라는 에러가계속 떠서 이유들을 찾아봤는데 적용들이 잘 안돼서 여러 시도들을 해봤다. npm을 다시 깔아보기도 하고 에디터를 다시 깔아보고여러 시도를 한 결과 npm install을 다시 한후에 npm update를하니까 문제가 해결됐다.

--- React/the_venue/src/components/header_footer/Footer.js
+++ React/the_venue/src/components/header_footer/Footer.js
@@ -4,7 +4,7 @@ import Fade from 'react-reveal/Fade';
 const Footer = () => {
     return (
         <footer className="bck_red">
-            <Fade delay={500}>
+            <Fade delay={300}>
                 <div className="font_righteous footer_logo_venue">2019.08.27</div>
                 <div className="footer_copyright">
                     포트폴리오

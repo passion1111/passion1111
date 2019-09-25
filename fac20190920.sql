72169f3641027652ab82271ce238a3cc09b02dce
passion1111 <se0572@naver.com>
2019-09-25 20:23:11 +0900

    sql inner join문같은경우 rownum을 어떻게 활용할까 생각했었는데 실행계획을 보고 이해한바로는 join을 실행하고나서 가장 바깥의 쿼리문을 비교한다는것을 알았다 그래서 rank over 나 rownum over함수를 이용해서 해결을하다보니생각보다 간단하게 join문들을 처리할수있다는것을 배웠고 join문들이 3개이상일경우 어떻게 처리해야할지는 생각해봐야할거같다

--- fac20190920.sql
+++ fac20190920.sql
@@ -10,8 +10,18 @@ select * from
 select * from (select (rank() over(order by Fac_serialnum))r ,FAC_NAME,FAC_ADDRESS,FAC_STATUS,FAC_CATEGORY from Facilities ) where r>0 and r<=7;
       delete from  Facilities where  fac_serialnum =7;                                                                                                        
 
-select * from  Facilities;
 
+select * from  facilities join facilities_inspection on facilities.fac_serialnum=facilities_inspection.facin_serialnum;
+
+select fac_serialnum ,rank() over(order by fac_serialnum) from  facilities join facilities_inspection on facilities.fac_serialnum=facilities_inspection.facin_serialnum;
+
+
+
+begin
+    insert into facilities_inspection values(:fac_period,:fac_name,fac_inspectiondate,
+
+
+select * 
 
 desc Facilities;
 insert into Facilities values((select count(Fac_serialnum)+1 from Facilities),'소화기','1층계단','정상','소방');

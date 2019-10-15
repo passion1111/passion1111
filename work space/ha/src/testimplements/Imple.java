d094a389b9c2bcde36a260fcbf371ffd361e034b
passion1111 <se0572@naver.com>
2019-10-16 03:02:21 +0900

    자바문제 당연하게 쓰고있던것중 하나인데 코딩을 도와주는 도구들은 추천 메소드들을불러와주는것이  return type들을확인하고 해당 파라메터타입으로 정해주기떄문에 관련된 메소드들을 불러올수있다고 생각하는데 타입파라미터를 어떻게 확정지어서 알수있는것일까 super token구현이 여기에 돼있는것이지 않을까하고 생각중이고 관련된 내용을 찾아봐야할거같다. getclass나 getreturntype으로 클래스나 타입만 알아내는것은 알아냈지만 사용가능한 method들을 추천해주는것이 어떤원리로 그러는것인지 찾아보자

--- work space/ha/src/testimplements/Imple.java
+++ work space/ha/src/testimplements/Imple.java
@@ -3,6 +3,9 @@ package testimplements;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -18,4 +21,26 @@ public class Imple {
 		assertEquals(3, ha2.hohoho());
 		assertEquals(5, ha2.hoho());
 	}
+	
+	@Test
+	public void iter() {
+		HashMap<String, String> hash=new HashMap<String, String>();
+		hash.put("1", "2");
+		hash.put("2","3");
+		Iterator<?> ha=hash.keySet().iterator();
+		int i=1;
+		while(ha.hasNext()) {
+			assertEquals("¸Â´Ï", hash.get(Integer.toString(i)), hash.get(ha.next()));
+			i+=1;
+		}
+	}
+	public HashMap<String, String> ha(){
+		HashMap<String, String> ha=new HashMap<String, String>();
+		return new HashMap<String, String>();
+	}
+	@Test
+	public void hashmethod() throws NoSuchMethodException, SecurityException {
+		System.out.println(ha().getClass());
+	}
+	
 }

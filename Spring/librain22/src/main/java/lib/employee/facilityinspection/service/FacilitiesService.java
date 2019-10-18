33513b42873edaa78217c34ddde5c049ce18b3e0
passion1111 <se0572@naver.com>
2019-10-18 16:15:07 +0900

    mybatis삽질 mybatis와 연결하고 dao에서  쿼리를 불러올려고해도 자꾸 binding exception이 떠서 무슨이유인지 몰라 검색을했지만 검색에서 나오는 해결책들은 나에게는 의미가 없었다. 그래서 찬찬히 처음부터 시작했는데 resulttype이 일단 dto가 아닌 dao로 돼있었고 namespace에 dao를 등록하지않아서 생긴 문제였다. junit으로 빠르게 테스트하지않았으면 2시간보다 더 많은 시간들이 걸렸지않을까 생각이 들었고 나중에 같은 실수를 하지않기위해서 남겨둔다.

--- Spring/librain22/src/main/java/lib/employee/facilityinspection/service/FacilitiesService.java
+++
@@ -1,9 +0,0 @@
-package lib.employee.facilityinspection.service;
-
-import java.util.List;
-
-import lib.employee.facility.model.FacilitiesDTO;
-
-public interface FacilitiesService {
-	public List<FacilitiesDTO> facselect(int Perpage,int page);
-}
--- library~3.sql
+++ library~3.sql
@@ -8,33 +8,67 @@ Fac_category varchar2(30)
 
 create table Facilities_inspection(   --시설물 점검
 FacIn_serialnum  number ,                           --점검한것 Facilities의 fk
-FacIn_name varchar2(30),    
+FacIn_name varchar2(30), 
+FacIn_address varchar2(30),
 FacIn_INSPECTION_DATE date default sysdate,    -- 점검일 
 FacIn_INSPECTION_DUE_DATE date
 );
+drop table Facilities_inspection;
 select * from Facilities_inspection;
 select * from Facilities;
-
 commit;
 update Facilities set fac_status='접수완료' ;
-create or replace procedure facin_insert( f_number IN Facilities_inspection.Facin_serialnum%TYPE,
+
+--시리얼넘버  대부분 시리얼 넘버로 가능한데  시설이름 주기 추가한 이유는 나중에 목록에 없는것을 추가했을시에 처리할려고. ,시설이름 ,주기  
+create or replace procedure facin_update( f_number IN Facilities_inspection.Facin_serialnum%TYPE,
                                           f_name   in facilities_inspection.facin_name%type,
+                                          f_address in facilities_inspection.facin_address%type,
                                           f_cycle   in number :=0)
 is
 begin
  case when f_name='엘레베이터' then 
-         update Facilities_inspection set FacIn_INSPECTION_DATE=sysdate,FacIn_INSPECTION_DUE_DATE=sysdate+90 where FacIn_serialnum=f_number;
+         update Facilities_inspection set FacIn_INSPECTION_DATE=sysdate,FacIn_INSPECTION_DUE_DATE=sysdate+90,facin_address=f_address where FacIn_serialnum=f_number;
       when f_name='계량기' then
-         update Facilities_inspection set FacIn_INSPECTION_DATE=sysdate,FacIn_INSPECTION_DUE_DATE=sysdate+30 where FacIn_serialnum=f_number;
+         update Facilities_inspection set FacIn_INSPECTION_DATE=sysdate,FacIn_INSPECTION_DUE_DATE=sysdate+30,facin_address=f_address where FacIn_serialnum=f_number;
       else
       for  temp_update in (select * from Facilities_inspection where FacIn_serialnum=f_number) --보기안좋으면 into로 대체
       loop
-      update Facilities_inspection set FacIn_INSPECTION_DATE=sysdate,FacIn_INSPECTION_DUE_DATE=sysdate+(temp_update.FacIn_INSPECTION_DUE_DATE-sysdate) where FacIn_serialnum=f_number;
+      update Facilities_inspection set FacIn_INSPECTION_DATE=sysdate,FacIn_INSPECTION_DUE_DATE=sysdate+(temp_update.FacIn_INSPECTION_DUE_DATE-sysdate),facin_address=f_address where FacIn_serialnum=f_number;
       end loop;
  
  end case;
 end;
 /
+select * from facilities_inspection;
+desc facilities_inspection;
+--insert into 하기위해
+desc Facilities_inspection;
+create or replace procedure facin_insert( f_number IN Facilities_inspection.Facin_serialnum%TYPE,
+                                          f_name   in facilities_inspection.facin_name%type,
+                                           f_address in facilities_inspection.facin_address%type,
+                                          f_cycle   in number :=0,
+                                          )
+is
+begin
+ case when f_name='엘레베이터' then 
+         insert into  Facilities_inspection(FACIN_SERIALNUM,FACIN_NAME,FACIN_INSPECTION_DATE,FACIN_SCHEDULED_INSPECTION_DATE,FACIN_STATUS,facin_address) 
+         values(select count(*)+1 from Facilities_inspection, '엘레베이터',sysdate,sysdate+30 ,'정상',f_address);
+      when f_name='계량기' then
+         insert into  Facilities_inspection(FACIN_SERIALNUM,FACIN_NAME,FACIN_INSPECTION_DATE,FACIN_SCHEDULED_INSPECTION_DATE,FACIN_STATUS,facin_address) 
+         values(select count(*)+1 from Facilities_inspection, '계량기',sysdate,sysdate+90 ,'정상',f_address);
+      else
+     insert into  Facilities_inspection(FACIN_SERIALNUM,FACIN_NAME,FACIN_INSPECTION_DATE,FACIN_SCHEDULED_INSPECTION_DATE,FACIN_STATUS,facin_address) 
+         values(select count(*)+1 from Facilities_inspection, f_name,sysdate,sysdate+f_cycle,'정상',f_address);
+ 
+ end case;
+end;
+/
+
+
+
+
+
+
         select * from Facilities_inspection;
 exec facin_insert(3,'프로시저테스트');
 desc Facilities_inspection;

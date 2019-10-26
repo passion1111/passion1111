e5cb337762bf6c485763173fe02bfd5064d8b79a
passion1111 <se072@naver.com>
2019-10-27 02:08:50 +0900

    인텔리j sts로  에러를 고쳐봤지만 도저히 고쳐지지않아서 인텔리j를 깔아서 스프링을 구동시킬려고했는데 이미 있는 프로젝트들은 왜안되는지모르겠는데 자꾸 에러가떠서 여러시도들을하다가 기본적인 컨트롤러들을 만들고 값만 체크할수있게 돌렸다 그동안 sts로만 개발을했던것인지 몰라도 아직 인텔리j가 굉장히 불편히 느껴져서 sts오류를 빠르게 잡고 해결을하던가  인텔리제이를 최대한 친숙하게쓰던 해야할거같다.

---
+++ Spring/testtest/src/test/java/test/test/test/testcontroller.java
@@ -0,0 +1,20 @@
+package test.test.test;
+
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Handles requests for the application home page.
+ */
+
+public class testcontroller {
+	
+	@Test
+	public void testname() {
+	}
+	
+	
+}

d8ded3a051ea83a84b0ba23b091fb2d74c1c34bd
passion1111 <se0572@naver.com>
2019-11-14 23:05:00 +0900

    공부한것 자기소개서를 쓰느라 많은시간을 다시 할애했고 만들었던 홈페이지들을 둘러보고있는데 어느사이트는 시작부터 jsessionid를 쿠키에 넣는데 어느사이트는 jsessionid를 쿠키에 넣지않는것을 확인했다. 그렇게해서 조금 더 찾아봤다. 이상하게 apache tomcat 레퍼런스에는 언제 발급하는지 적혀있지않아서 stackoverflow를 찾아보니 jsessionid가 생성되고 발급해주는 순간은 httpservletrequest에서 getsession을 하거나 setsession을 하는순간 발급해준다. 이떄 getsession을 할떄 null이여도 발급을 해주기떄문에 세션을 가져오거나 설정하는 컨트롤러를 타는순간 jsessionid를 발급해준다는것을 알았고 그외에 내가 찾아낸 방법은 httpsession객체가 파라미터에 추가하는순간 만들어진다는것을 알았다. 그래서 정리하면 jesessionid가 방급되는 순간은크게 3가지로 볼수있다. 1.request.getsession할떄 2.request.setsession할떄 3.httpservlet 객체가 생성될떄 어제 공부하고자한것을 미흡하게 했으니 자기전에 어제공부해야할것들을 다시집중해서 만들어야겠다.

--- Spring/Test/src/main/java/test/test/test/HomeController.java
+++ Spring/Test/src/main/java/test/test/test/HomeController.java
@@ -4,6 +4,11 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -23,16 +28,31 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model,HttpServletRequest request) {
+		Cookie cookie=new Cookie("name","name");
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		cookie.setMaxAge(10000000);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		return "home";
+	}
+	
+	
+	@RequestMapping(value = "/host", method = RequestMethod.GET)
+	public String home2(Locale locale, Model model,HttpServletResponse response) {
+		Cookie cookie=new Cookie("name","name");
+		logger.info("Welcome home! The client locale is {}.", locale);
+		cookie.setMaxAge(10000000);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
+		String formattedDate = dateFormat.format(date);
+		response.addCookie(cookie);
+		model.addAttribute("serverTime", formattedDate );
 		return "home";
 	}
 	

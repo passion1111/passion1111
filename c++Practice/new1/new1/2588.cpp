0a117af5cf545399747bc0fb958582adcaa599d4
passion1111 <se0572@naver.com>
2019-12-26 22:30:02 +0900

    공부한것  기초적인 C++문법을 배웠고 11문제정도만 풀어봤던것같다 기본적인 입출력에대해서는 어느정도 알게됐는데자바랑 다른것이 많아서 초반에는 약간 삽질을 했었다 그리고  자바스크립트와 자바도 틈틈히공부하고있는데 파일업로드과정을 제대로  만드는것도 쉽지만은않다는것을 다시한번 느꼈고 여러번 만들어보고 다르게  만들어보면서 실력을 키워야겠다는 생각을 했다

---
+++ c++Practice/new1/new1/2588.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a * (b % 10) << endl;
+	cout << a * (b % 100 - b % 10) /10 << endl;
+	cout << a * (b%1000	 -( b % 100 ))/100  << endl;
+	cout << a * b << endl;
+
+
+
+	return 0;
+}
---
+++ c++Practice/new1/new1/backjoon10718.cpp
@@ -0,0 +1,9 @@
+
+
+int main() {
+
+
+	std::cout << "Hello World!";
+
+	return 0;
+}

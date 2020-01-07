7b4c933c91b6667c0692fa04c8d67013df129726
passion1111 <se0572@naver.com>
2020-01-07 23:16:35 +0900

    공부한것  다시 간단한 DP문제를 풀며 감을 잡고있는데 복습을별로안해서그런지  습득하는것이 쉽지가않다는것을 느꼈다. 아는것만을 공부하는것이아니라  모르는것을 의식적으로 연습할려고해야 실력이느는데 아직 그부분에대해서는  학습이 부족한것을 느끼고있다. 이것을 해결할수있는 전략을짜는것이  급선무일것같다. 그래도 꾸준히 코딩을하고있으니 이것을 이용해서  타개할방법을 찾는것이 좋지않을까 싶다.

---
+++ c++Practice/DP/DP/11722.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void) {
+    int N;
+    int Dp[1010] = {};
+    int A[1010] = {};
+    int max = 0;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++)
+        scanf("%d", &A[i]);
+
+    for (int i = 1; i <= N; i++)
+    {
+        int minDp = 0;
+        for (int j = 0; j < i; j++) {
+   
+ //    A[3]=3,1,2;
+//    minDP는 매번0으로 초기화 
+     //i=0일떄      j=0   min값 변화X           DP[i]=0+1;
+    //i=1일떄        1    3      minDP  0    DP[j] =1     해당 
+            //     minDP=1 그후 DP[1]=2;
+            if (A[i] < A[j]) {     
+
+
+
+
+
+
+                if (minDp < Dp[j])
+                    minDp = Dp[j];
+            }
+        }
+        Dp[i] = minDp + 1;
+        if (max < Dp[i])
+            max = Dp[i];
+    }
+
+    printf("%d\n", max);
+
+}

4bc95e52fcf232e2ff130469e011dae6b560d05e
passion1111 <se0572@naver.com>
2020-01-11 23:37:49 +0900

    공부한것  백준문제하나를 풀었는데 이상하게 로직에 이상이 없는데도 틀렸다고 하길래  도대체 뭐가문제인가하고 답지 코드와 한줄한줄 바꿔가며 옮겨갔는데  똑같은 텍스트문서이고 	비교해가며 바꿔갔는데 틀렸다고하길래  무슨문제인가하고 쳐다봤더니 도대체 차이가없는데도 에러가뜨는거고  하고쳐다봤는데 똑같은 텍스트문자열인데 어떤것은 에러가뜨고 어떤것은  안떠서 계속해서 뭐가다른지 하루종일 쳐다봤었는데 도저히 찾지못했었다.  그리고 그냥 지우고 다시 적어서 제출하니 맞았습니다가 떠서 아직  무슨이유인지 정확히 파악하지못한상태이고 이것떄문에 짜증이 많이났었는데  정답코드와 비교해서 텍스트가 완전히같은데도 에러코드가 나는것을봐서는  인코딩방식때문에 에러가 발생한거아니면 설명이 힘들어질것같다

---
+++ c++Practice/new1/new1/test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <string>
+
+ 
+using namespace std;
+int main() {
+
+    string ab;
+    getline(cin, ab);
+    int a = 33;
+    ab = ab +to_string(a);
+   
+
+    cout << ab << endl;
+
+
+}

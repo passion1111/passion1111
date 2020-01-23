851151175a3fcb145f5bd843771055130d5f1319
passion1111 <se0572@naver.com>
2020-01-23 20:13:08 +0900

    공부한것  자바 1.7버전이하는 tls1.1버전 이하를 쓰는데 maven repository에서  2020년 1월13일부로 더이상 tls1.1이하를 지원안하게돼서 https로만  요청을해야한다 그것을 해결하기위해서는 jvm에서 설정을 바꾸거나 maven  install할떄-dhttps명령어를 이용하거나 자바버전을 그냥 편하게 1.8으로  올리는방법이 있는데 1.8버전으로 올릴수없는 여의치않는 상황이면  jvm이나 maven또는 프로그램 시작시 setproperty로 httpsprotocol을  정해주면된다 근데 나같은경우 이상하게 저 세가지 방법을 모두사용했는데도  https로 요청을하라는 501에러가 하루종일떠서 무엇이 문제인지 파악하기가  힘든하루를 보냈다.
      그리고 스프링을 오래공부하지않다보니 controller가 기본 설정자체가
      component로 설정돼있다는것과 component scan 하위 패키지는 모두
      검사한다는것을 까먹고있었다 다시 기본서를 읽어가면서 스프링에대한
      개념을 탄탄히 잡아야할것같고 property도 여러가지를 정하고싶으면
      <list> <value></value> <value></value></list>형태로 보내주면
      여러가지 데이터를 담을수있다는것을 망각하고있었다.
    
       네트워크에대한 공부도 다시복습을했고 지하철을타면서는
       알고리즘기초문제들에대해서 다시 생각해보는 시간을가졌다.
       벨만포드 알고리즘은 아직 코드로 이해하기에는 멀은것같아서
       기초알고리즘부터 탄탄히잡고 공부해야할것같다

--- Spring/TestNullcheck/src/nullcheck/Nullcheck.java
+++ Spring/TestNullcheck/src/nullcheck/Nullcheck.java
@@ -6,11 +6,15 @@ public class Nullcheck {
 	
 	public static void main(String[] args) {
 		String[] check= null;
-		System.out.println(check==null);
-		if(check==null) {
-			System.out.println(String.valueOf(check));
-			check[0]=String.valueOf(check);
-			System.out.println(check[0]);
-		}
+//		check[0]='';
+//		System.out.println(check==null);
+//		if(check==null) {
+//			System.out.println(String.valueOf(check));
+//			check[0]=String.valueOf(check);
+//			System.out.println(check[0]);
+//		}
+		
+		
+		System.out.println(String.valueOf(1));
 	}
 }

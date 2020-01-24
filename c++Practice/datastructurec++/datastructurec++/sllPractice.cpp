101cc9e776989d2c8ffb78b157a1a30ebbef16ed
passion1111 <se0572@naver.com>
2020-01-24 23:13:47 +0900

    공부한것 안다고 생각하고 구현하지않고있던것들을 직접 구현해보니 막히는것이 많아 안다고 생각하는게 아는게아니라 실제로 풀어봐서 알고있는것을 안다고 생각해야하고 그럴려면 실제로 실습이나 문제들을 설명을하면서 출력을해야한다는것을 알고리즘공부를하면서 많이꺠닫고있다. 그래서 오늘은 spring io에들어가서 batch에대한 문서도 읽어가면서 영어문서읽는것에대한 두려뭉을 낮추고있고 링크드리스트를 직접다시구현해보면서  막히는것들을 학습했다. 이제 학습시간에 많은시간을 복습에 투자하면서 뭔가 빠져나가는공부가아니라 채워나가는공부를해야겠다고 생각이 들었다. 내일은 알고리즘을 책위주로 공부해서 이론을 채워나가고 나머지시간에는 네트워크,운영체제책들을 보면서 전공지식을 조금 더 채워나가야겠다

---
+++ c++Practice/datastructurec++/datastructurec++/sllPractice.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct tagName {
+	int data;
+	struct tagName* NextNode;
+}Node;
+
+
+
+Node* sllcreatenode(int data) {
+	Node* Head = (Node*)malloc(sizeof(Node));
+	Head->data = data;
+	Head->NextNode = NULL;
+
+
+	return Head;
+};
+
+
+void sllinsertnode(Node** list,Node* node) {
+	
+	if ( (*list) == NULL) {
+		(*list) = node;
+	}
+	else {
+
+		Node* tail = (*list);
+		while ((*list)->NextNode != NULL) {
+			tail = tail->NextNode;
+		}
+		tail->NextNode = node;
+	}
+
+};
+
+void printnode(Node* list) {
+	if (list == NULL) {
+		return;
+	}
+	while (list != NULL) {
+		printf("%d\n", list->data);
+		list = list->NextNode;
+
+	}
+}
+
+	int main() {
+		Node* list;
+		printf("das");
+		list=sllcreatenode( 6);
+		Node* first(sllcreatenode(33));
+		sllinsertnode(&list, first);
+		printnode(list);
+
+
+
+	
+
+};

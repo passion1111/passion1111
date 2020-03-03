2ceb2875d4ccc5a36aa843551dd35382e01ff62a
passion1111 <se0572@naver.com>
2020-03-04 06:18:04 +0900

    공부한것 자료구조 구현에 다시 힘쓰고있다. 심리적인 저항이 생각보다있어서 많이나가지는 못했어도 구현부분을 지우고 다시 함수를 작성해보니 그래도 링크드리스트는 쉽게 구현할수있는 수준까지는 올라온것같다. 머리로 아는것과 실제로 구현해는것의 큰차이를 다시한번 느끼고있는중이다. 더 공부해서 나머지내용들도 구현해나가는것을 일단 목표로 잡고있다

---
+++ c++Practice/CircularLinkedList/CircularLinkedList/CircularDoublyLinkedList.c
@@ -0,0 +1,105 @@
+#include "CircularDoublyLinkedList.h"
+
+Node* CDLL_CreateNode(ElementType NewData) {
+    Node* NewNode = (Node*)malloc(sizeof(Node));
+    NewNode->Data = NewData;
+    NewNode->NextNode = NULL;
+    NewNode->PrevNode = NULL;
+
+
+    return NewNode;
+}
+void  CDLL_DestroyNode(Node* Node) {
+    free(Node);
+}
+void  CDLL_AppendNode(Node** Head, Node* NewNode) {
+
+    if ((*Head) == NULL) {
+        *Head = NewNode;
+        (*Head)->NextNode = NewNode;
+        (*Head)->PrevNode = NewNode;
+    }
+    else {
+        Node* tail = *Head;
+        tail->PrevNode->NextNode = NewNode;
+        NewNode->PrevNode = tail->PrevNode;
+        NewNode->NextNode = tail;
+        tail->PrevNode = NewNode;
+    }
+
+
+}
+void  CDLL_InsertAfter(Node* Current, Node* NewNode) {
+
+    Current->NextNode->PrevNode = NewNode;
+    NewNode->PrevNode = Current;
+    NewNode->NextNode = Current->NextNode;
+    Current->NextNode = NewNode;
+
+
+}
+void  CDLL_RemoveNode(Node** Head, Node* Remove) {
+
+    if ((*Head) == Remove) {
+        (*Head)->PrevNode->NextNode = Remove->NextNode;
+        (*Head)->NextNode->PrevNode = Remove->PrevNode;
+        (*Head) = Remove->NextNode;
+    }
+    else {
+        Node* tail = *Head;
+
+
+        while (tail != NULL && tail->NextNode != Remove) {
+            tail = tail->NextNode;
+        }
+        if (tail != NULL) {
+            tail->NextNode = Remove->NextNode;
+            tail->NextNode->PrevNode = Remove->PrevNode;
+            Remove->NextNode = NULL;
+            Remove->PrevNode = NULL;
+
+
+        }
+
+
+    }
+
+
+}
+Node* CDLL_GetNodeAt(Node* Head, int Location) {
+    Node* tail = Head;
+
+    while (tail != NULL && (--Location) >= 0 && tail->NextNode !=Head) {
+        tail = tail->NextNode;
+    }
+
+
+    return tail;
+
+}
+int   CDLL_GetNodeCount(Node* Head) {
+    Node* tail = Head;
+    int count = 0;
+
+    while (tail != NULL && tail->NextNode !=Head) {
+        tail = tail->NextNode;
+        count++;
+    }
+
+    return count;
+}
+
+void PrintNode(Node* _Node)
+{
+    if (_Node->PrevNode == NULL)
+        printf("Prev: NULL");
+    else
+        printf("Prev: %d", _Node->PrevNode->Data);
+
+    printf(" Current: %d ", _Node->Data);
+
+    if (_Node->NextNode == NULL)
+        printf("Next: NULL\n");
+    else
+        printf("Next: %d\n", _Node->NextNode->Data);
+}
